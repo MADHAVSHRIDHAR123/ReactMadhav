2629a29120edaf6e67b2d503368379064e1ae048
MADHAVSHRIDHAR123 <madhav.appic@gmail.com>
2024-11-13 20:54:49 +0530

    push

---
+++ src/useState.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  console.log(count, "initial value");
+  console.log(count, "counttttt");
+
+  const increament = () => {
+    if (count < 10) {
+      setCount((prev) => prev + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  const styleCounter = {
+    color: count > 5 ? "red" : "green",
+  };
+  // if ( count > 5 ) {
+  //     color = 'red'
+  // }else{
+  // color='green'
+
+  // }
+  return (
+    <div>
+      <h1 style={styleCounter}>Counter : {count}</h1>
+      <button onClick={increament}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      {/* <button onClick={}></button> */}
+    </div>
+  );
+};
+
+export default Counter;
