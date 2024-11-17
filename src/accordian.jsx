c70004f6f0dfcac36c8ecad1c5f95f00a45006de
MADHAVSHRIDHAR123 <madhav.appic@gmail.com>
2024-11-17 14:51:18 +0530

    Accordin and get api

--- src/accordian.jsx
+++ src/accordian.jsx
@@ -1,17 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React  from "react";
-import data from "./data";
+// import data from "./data";
 import './App.css'
 
 export default function Accordian (){
 const [selected, setSelected]= useState(null)
 const [enableMutiselection, setEnableMutiselection] = useState(false)
 const [multiple, setmulitple] = useState([])
+const [data, setdata] = useState([])
 // const data = []
-// function handlesingleSelection(currentId) {
-//     setSelected(currentId=== selected ? null)
-// }
-console.log(data, "daataaa")
+function handlesingleSelection(currentId) {
+    setSelected(currentId=== selected  ? null: currentId)
+}
+
+function handleMultpleSelection(currentId){
+    const coyArray = [...multiple]
+    const findSelectedinDex = multiple.indexOf(currentId)
+
+    
+
+    if(findSelectedinDex !== -1){
+     coyArray.splice(findSelectedinDex,1)
+        
+    }else{
+        coyArray.push(currentId)
+    }
+    setmulitple(coyArray)
+    console.log(coyArray,enableMutiselection, "daataaa")
+}
+
+    async function fetchData (){
+       const data =  await fetch("https://jsonplaceholder.typicode.com/posts")
+
+
+       setdata(await data.json())
+     }
+
+useEffect(()=>{
+    fetchData();
+},[])
+
     return(
 
        <div className="acc-wrapper">
@@ -20,11 +48,17 @@ console.log(data, "daataaa")
             {
                 data?.map((dataiItem) => (
                     <div className="item">
-                        <div  className="title">
-                        <h3>{dataiItem.question}</h3>
+
+                        <div  className="title" onClick={enableMutiselection?()=>handleMultpleSelection(dataiItem.id):()=>handlesingleSelection(dataiItem.id)}>
+                        <h3>{dataiItem.title}</h3>
                         <span>+</span>
                         </div>
-                        </div>
+                        <div>
+                            {enableMutiselection? (multiple.indexOf(dataiItem.id) !== -1 && (<div>{dataiItem.body}</div>)):
+                            (selected === dataiItem.id && (<div>{dataiItem.body}</div>))}
+                            </div>
+                        </div>     
+                        
                 )
             )}
     
